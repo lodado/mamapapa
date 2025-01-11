fa1acc25ba19dc8f64498092bafc41ba94f803a6
chungheon_yi <ycp998@naver.com>
2025-01-11 19:14:59 +0900

    chore: 글자 수정

--- src/features/AiModel/ui/components/ModelDownloadSuccess.tsx
+++ src/features/AiModel/ui/components/ModelDownloadSuccess.tsx
@@ -12,7 +12,7 @@ const ModelDownloadSuccess = () => {
 
       <div className="mr-[1.75rem]">
         <p className="text-text-success subhead-2">비교를 위한 AI 모델을 다운로드 완료</p>
-        <p className="body-1 text-text-03">다운로드가 완료되었습니다.(100%)</p>
+        <p className="body-1 text-text-03">다운로드가 완료되었습니다. (100%)</p>
       </div>
     </>
   );
