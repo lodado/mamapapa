12dd99330e449ac15f736f5e3ac296de42e19810
chungheon_yi <ycp998@naver.com>
2025-01-12 00:41:43 +0900

    fix: 쓸모없는 ssg 삭제

--- src/app/[locale]/faces/[id]/page.tsx
+++ src/app/[locale]/faces/[id]/page.tsx
@@ -1,11 +1,13 @@
 import { products } from "../product";
 import { ReactNode } from "react";
 
+/* 
 export async function generateStaticParams() {
   return products.map((product) => ({
     id: product.id,
   }));
 }
+*/
 
 export default async function ProductPage() {
   return <></>;
