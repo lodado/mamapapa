692f759e4c2ea32e6e3cbc688c41127a811e71c2
lodado <ycp998@naver.com>
2025-01-19 23:15:38 +0900

    chore: css 변경

--- src/shared/ui/ReactiveLayout/ReactiveLayout.tsx
+++ src/shared/ui/ReactiveLayout/ReactiveLayout.tsx
@@ -2,10 +2,8 @@ import React, { PropsWithChildren } from "react";
 
 const ReactiveLayout = ({ children }: PropsWithChildren) => {
   return (
-    <div className="flex w-full h-max items-center justify-center bg-background-03 ">
-      <div className="w-full min-h-screen bg-background-01 md:w-[768px] mx-auto flex flex-col items-center">
-        {children}
-      </div>
+    <div className="flex w-full h-max min-h-screen items-center justify-center bg-background-03 ">
+      <div className="w-full h-full bg-background-01 md:w-[768px] mx-auto flex flex-col items-center">{children}</div>
     </div>
   );
 };
