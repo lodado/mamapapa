3d591391faf563013ce9b2299090865fe9fcf780
lodado <ycp998@naver.com>
2025-02-13 21:37:44 +0900

    chore: manifest update

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -6,10 +6,25 @@ export default function manifest(): MetadataRoute.Manifest {
     short_name: "Simmey",
     description: "Use a face-matching AI to see how much you resemble your mom and dad!",
     theme_color: "#2470FF",
-    background_color: "#FFFFFF",
+    background_color: "#ffffff",
     display: "standalone",
     orientation: "portrait",
     scope: "/",
     start_url: "/?app=pwa",
+
+    icons: [
+      {
+        purpose: "maskable",
+        sizes: "75x75",
+        src: "/Logo.svg",
+        type: "image/svg",
+      },
+      {
+        purpose: "maskable",
+        sizes: "150x150",
+        src: "/Logo.svg",
+        type: "image/svg",
+      },
+    ],
   };
 }
