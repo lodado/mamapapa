5c87967d185d27bd3f8fda6d7e0752c2a1406fbc
lodado <ycp998@naver.com>
2025-03-01 23:29:42 +0900

    chore: upload bug 수정

--- src/shared/constants/constant.ts
+++ src/shared/constants/constant.ts
@@ -1,6 +1,6 @@
 export const ROOT_URL: string = "";
 
-export const USER_ID_SPLITTER: string = `${process.env.AUTH_URL}@`;
+export const USER_ID_SPLITTER: string = `${process.env.KAKAO_ID!.substring(0, 7)}@`;
 
 export const IS_DEPLOYMENT = !!process.env.VERCEL_URL;
 
