af8b2e261139a18e5965631ad6561464f4d94a5c
ChungHeonLee <ycp998@naver.com>
2025-05-26 17:12:39 +0900

    chore: update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,8 +1,12 @@
-import { PAGE_ROUTE } from "@/entities/Router/configs/route";
 import { i18nOption } from "@/shared";
 
 const webUrl = process.env.NEXT_PUBLIC_CLIENT_URL!;
 const defaultImage = "/Logo.svg";
+
+export const PAGE_ROUTE = {
+  MAIN: "/",
+  FACES: "/faces",
+} as const;
 
 export default function sitemap() {
   return [
@@ -13,16 +17,14 @@ export default function sitemap() {
         changeFrequency: "monthly" as const,
         priority: Math.max(0.5, 0.99 - index * 0.1),
 
-        /* 
         alternates: {
           languages: {
-        ...i18nOption.locales.reduce((total: any, locale) => {
-          total[locale] = `${webUrl}/${locale}${path}`;
-          return total;
-        }, {}),
+            ...i18nOption.locales.reduce((total: any, locale) => {
+              total[locale] = `${webUrl}/${locale}${path}`;
+              return total;
+            }, {}),
           },
         },
-        */
       };
     }),
   ];
